4e93ad9eb451f9a4d9308cabd71a1bcb0f4ffb7b
Telluks <rhacher76.rq@gmail.com>
2025-05-15 14:28:02 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-import os
-
-# Токен от @BotFather
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-# Админы: список ID через запятую, например "123456789,987654321"
-ADMIN_IDS = list(map(int, os.environ.get("ADMIN_IDS", "").split(",")))
+BOT_TOKEN = "токен_твоего_бота"
+ADMIN_IDS = [твой_телеграм_id, id_менеджера]  # список ID в int
